c1a9817d164dce70d98a768a0da3ab78eabc54cf
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-08-17 23:43:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ async function connectToWA() {
 
       const up = `DANUWA-MD connected ✅\n\nPREFIX: ${prefix}`;
       await danuwa.sendMessage(ownerNumber[0] + "@s.whatsapp.net", {
-        image: { url: `https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true` },
+        image: { url: `https://files.catbox.moe/37jcmn.jpg` },
         caption: up
       });
 
@@ -179,7 +179,7 @@ async function connectToWA() {
 ensureSessionFile();
 
 app.get("/", (req, res) => {
-  res.send("Hey, DANUWA-MD started✅");
+  res.send("Hey, Anubis started✅");
 });
 
 app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
