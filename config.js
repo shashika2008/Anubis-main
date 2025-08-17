4a0b7c0181aaec5c7ec800d0b314623158af9a05
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-08-17 23:05:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
+SESSION_ID: process.env.SESSION_ID || "Tf4SWC4Q#ItlmzCE8lKgLv9Mv-4dbm1XOfwFxNJlGs-HZkozXVvU",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/wy8i4k.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+BOT_OWNER: '94705104830',  // Replace with the owner's phone number
 
 
 
