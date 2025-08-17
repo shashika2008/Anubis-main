3dddd47a9c886dfce3f1163f92796fb41cc87f72
Mr shashika dilshan <dmritasudarsani@gmail.com>
2025-08-17 23:36:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,24 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Tf4SWC4Q#ItlmzCE8lKgLv9Mv-4dbm1XOfwFxNJlGs-HZkozXVvU",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/wy8i4k.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94705104830',  // Replace with the owner's phone number
-
-
-
+    SESSION_ID: process.env.SESSION_ID || "Tf4SWC4Q#ItlmzCE8lKgLv9Mv-4dbm1XOfwFxNJlGs-HZkozXVvU",
+    
+    // Alive page image (can be any URL)
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/wy8i4k.jpg",
+    
+    // Alive message text
+    ALIVE_MSG: process.env.ALIVE_MSG || `╭────────────────────────────╮
+│          ✧•    Anubis •✧       │
+│╭──────────────⋆⋅☆⋅⋆─────────────╮│
+││ • Ｓｔａｔｕｓ : 𝔸𝕃𝕀𝕍𝔼 ✨          ││
+││ • ☣️ 𝕄𝕒𝕕𝕖 𝔹𝕪 : shashika        ││
+││ • ☣️ status : online      ││
+││ • ☣️ Vibe : 𒀭𒀮𒀯𒆙𒆜 100% good││
+│╰──────────────⋆⋅☆⋅⋆─────────────╯│
+╰────────────────────────────╯`,
+    
+    // Bot owner phone number
+    BOT_OWNER: '94705104830',
 };
